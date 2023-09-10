19095710bed9ed867a340d80f9f6297afa0a5b75
rayhannn11 <rayhan.onepiece@gmail.com>
2023-09-11 00:05:56 +0700

    third  Commit

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -24,6 +24,20 @@ class App {
     const page = routes[url];
     this._content.innerHTML = await page.render();
     await page.afterRender();
+
+    // Debug Skip Content Issue
+    const exploreRestaurant = document.querySelector("#explore-restaurant");
+    const mainContent = document.querySelector("#main-content");
+    const skipLink = document.querySelector(".skip-link");
+    skipLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (exploreRestaurant) {
+        exploreRestaurant.scrollIntoView({ behavior: "smooth" });
+      } else {
+        mainContent.scrollIntoView({ behavior: "smooth" });
+      }
+      skipLink.blur();
+    });
   }
 }
 
